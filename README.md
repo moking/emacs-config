08b5a0aececb8baf0a2541866118e425138584b9
Fan Ni <nifan.cxl@gmail.com>
2024-01-05 18:32:12 -0800

    delete the old config

--- README.md
+++
@@ -1,2 +0,0 @@
-fork from http://tuhdo.github.io/c-ide.html
-add some personal configurations and packages.
--- elpa/dummyparens-20141009.324/dummyparens-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dummyparens" "20141009.324" "parenthesis auto-pairing and wrapping" 'nil :url "https://github.com/snosov1/dummyparens" :keywords '("dummyparens" "auto-pair" "wrapping"))

880eeb1d550871fa14dec35657f18d027cf7c30c
Fan Ni <nifan.cxl@gmail.com>
2024-01-05 18:43:57 -0800

    emacs used in 1/5/2024, works for evil,vterm,markdown mode and ect.

---
+++ emacs.d/elpa/recently-20210930.207/recently-pkg.el
@@ -0,0 +1,15 @@
+(define-package "recently" "20210930.207" "Track recently opened files to visit them again"
+  '((cl-lib "0.5")
+    (emacs "24"))
+  :commit "94b31f6bf1dab6af942948fec975e37424938a62" :authors
+  '(("10sr <8.slashes [at] gmail [dot] com>"))
+  :maintainers
+  '(("10sr <8.slashes [at] gmail [dot] com>"))
+  :maintainer
+  '("10sr <8.slashes [at] gmail [dot] com>")
+  :keywords
+  '("utility" "files")
+  :url "https://github.com/10sr/recently-el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
